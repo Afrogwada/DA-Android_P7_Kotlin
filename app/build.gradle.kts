c9cb84c89f24cf9b28f68460b8cab450f57d22a2
Afrogwada <afrogwadaweb@gmail.com>
2026-02-06 16:03:33 +0100

    correction problème gradle (kapt('androidx.room:room-compiler:2.4.3'))

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,8 @@ android {
 dependencies {
     //Room
     implementation("androidx.room:room-runtime:2.4.3")
-    implementation("androidx.room:room-compiler:2.4.3")
+    //implementation("androidx.room:room-compiler:2.4.3")
+    kapt("androidx.room:room-compiler:2.4.3")
     implementation("androidx.room:room-ktx:2.4.3")
 
 
